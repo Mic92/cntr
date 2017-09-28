44b456b61fc2a46f128152c1ff56816a428e49d9
Joerg Thalheim <joerg@thalheim.io>
2017-09-28 13:48:07 +0100

    add large file support

--- src/statvfs.rs
+++ src/statvfs.rs
@@ -3,8 +3,8 @@ use nix::{Result, Errno};
 use std::mem;
 use std::os::unix::io::RawFd;
 
-pub fn fstatvfs(fd: RawFd) -> Result<libc::statvfs> {
+pub fn fstatvfs(fd: RawFd) -> Result<libc::statvfs64> {
     let mut s = unsafe { mem::zeroed() };
-    let res = unsafe { libc::fstatvfs(fd, &mut s) };
+    let res = unsafe { libc::fstatvfs64(fd, &mut s) };
     Errno::result(res).map(|_| s)
 }
