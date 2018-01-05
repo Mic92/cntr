520a22f7e11f1fba065f07655eba2ecdfe984932
Joerg Thalheim <joerg@thalheim.io>
2018-01-05 11:39:31 +0000

    fix compilation with musl libc

--- src/ioctl.rs
+++ src/ioctl.rs
@@ -3,17 +3,38 @@ use nix::Result;
 use nix::errno::Errno;
 use std::os::unix::io::RawFd;
 
+#[cfg(not(any(target_env = "musl")))]
 pub fn ioctl_read(fd: RawFd, cmd: c_ulong, data: &mut [u8]) -> Result<()> {
     let res = unsafe { libc::ioctl(fd, cmd, data) };
     Errno::result(res).map(drop)
 }
 
+#[cfg(any(target_env = "musl"))]
+pub fn ioctl_read(fd: RawFd, cmd: c_ulong, data: &mut [u8]) -> Result<()> {
+    let res = unsafe { libc::ioctl(fd, cmd as i32, data) };
+    Errno::result(res).map(drop)
+}
+
+#[cfg(not(any(target_env = "musl")))]
 pub fn ioctl_write(fd: RawFd, cmd: c_ulong, data: &[u8]) -> Result<()> {
     let res = unsafe { libc::ioctl(fd, cmd, data) };
     Errno::result(res).map(drop)
 }
 
+#[cfg(any(target_env = "musl"))]
+pub fn ioctl_write(fd: RawFd, cmd: c_ulong, data: &[u8]) -> Result<()> {
+    let res = unsafe { libc::ioctl(fd, cmd as i32, data) };
+    Errno::result(res).map(drop)
+}
+
+#[cfg(not(any(target_env = "musl")))]
 pub fn ioctl(fd: RawFd, cmd: c_ulong) -> Result<()> {
     let res = unsafe { libc::ioctl(fd, cmd) };
+    Errno::result(res).map(drop)
+}
+
+#[cfg(any(target_env = "musl"))]
+pub fn ioctl(fd: RawFd, cmd: c_ulong) -> Result<()> {
+    let res = unsafe { libc::ioctl(fd, cmd as i32) };
     Errno::result(res).map(drop)
 }
