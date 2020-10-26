39239e87cc581ded2baa73ab7e3ea1557bc443c5
Jörg Thalheim <joerg@thalheim.io>
2020-10-26 13:33:26 +0100

    fix musl build

--- src/sys_ext/internal/setrlimit.rs
+++ src/sys_ext/internal/setrlimit.rs
@@ -4,7 +4,14 @@ use libc;
 use nix;
 use nix::errno::Errno;
 
+#[cfg(not(any(target_env = "musl")))]
 pub fn setrlimit(resource: libc::c_uint, rlimit: &Rlimit) -> nix::Result<()> {
+    let res = unsafe { libc::setrlimit64(resource, rlimit as *const Rlimit) };
+    Errno::result(res).map(drop)
+}
+
+#[cfg(any(target_env = "musl"))]
+pub fn setrlimit(resource: libc::c_int, rlimit: &Rlimit) -> nix::Result<()> {
     let res = unsafe { libc::setrlimit64(resource, rlimit as *const Rlimit) };
     Errno::result(res).map(drop)
 }
