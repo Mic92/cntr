6fd6f5a1bd575bde4c2916f08fb977874a4d2128
Jörg Thalheim <joerg@thalheim.io>
2020-10-26 14:50:10 +0100

    scripts/sign-drone.sh: fix loading secrets

--- scripts/sign-drone.sh
+++ scripts/sign-drone.sh
@@ -1,10 +1,11 @@
 #!/usr/bin/env nix-shell
 #!nix-shell -i bash -p bash -p drone-cli -p sops
-set -euo pipefail
+set -xeuo pipefail
 
-eval "$(sops -d --output-type dotenv ./drone-secrets.yml)"
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+
+eval "$(sops -d --output-type dotenv ${DIR}/drone-secrets.yml)"
 cd "$DIR"/..
 export DRONE_SERVER=https://drone.thalheim.io DRONE_TOKEN
-drone sign Mic92/dotfiles --save
+drone sign Mic92/cntr --save
 git add .drone.yml
