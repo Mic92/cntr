Mic92/cntr

commit 4aa6c94b9678fc45f6ee4606dea4365dd77c92da

get rid of tempfile crate

src/dotcntr.rs
@@ -8,12 +8,13 @@ use std::os::unix::prelude::*;
 
 use crate::capabilities;
 use crate::procfs::ProcStatus;
+use crate::tmp;
 use crate::types::{Error, Result};
 
 /// Hidden directory with CAP_CHROOT enabled cntr-exec binary
 pub struct DotcntrDir {
     pub file: File,
-    pub dir: tempfile::TempDir,
+    pub dir: tmp::TempDir,
 }
 
 impl DotcntrDir {
@@ -48,7 +49,7 @@ impl DotcntrDir {
 }
 
 pub fn create(process_status: &ProcStatus) -> Result<DotcntrDir> {
-    let dotcntr_dir = tryfmt!(tempfile::tempdir(), "failed to create temporary directory");
+    let dotcntr_dir = tryfmt!(tmp::tempdir(), "failed to create temporary directory");
     let dotcntr_fd = tryfmt!(
         fcntl::open(
             dotcntr_dir.path(),

src/mountns.rs
@@ -11,6 +11,7 @@ use std::path::{Path, PathBuf};
 use crate::fs::CntrFs;
 use crate::ipc;
 use crate::namespace;
+use crate::tmp;
 use crate::types::{Error, Result};
 
 pub struct MountNamespace {
@@ -39,9 +40,9 @@ impl MountNamespace {
         let path = PathBuf::from("/tmp");
         tryfmt!(mkdir_p(&path), "failed to create /tmp");
 
-        let mountpoint = tryfmt!(tempfile::tempdir(), "failed to create temporary mountpoint");
+        let mountpoint = tryfmt!(tmp::tempdir(), "failed to create temporary mountpoint");
 
-        let temp_mountpoint = tryfmt!(tempfile::tempdir(), "failed to create temporary mountpoint");
+        let temp_mountpoint = tryfmt!(tmp::tempdir(), "failed to create temporary mountpoint");
 
         tryfmt!(
             sched::unshare(CloneFlags::CLONE_NEWNS),

Cargo.toml
@@ -12,7 +12,6 @@ license = "MIT"
 cpuprofiler = { version = "0.0.*", optional = true }
 argparse = "0.2.*"
 log = "0.4.*"
-tempfile = "3"
 num_cpus = "1.13.*"
 libc = "0.2.*"
 parking_lot = "0.11.*"

src/lib.rs
@@ -27,6 +27,7 @@ mod procfs;
 mod pty;
 mod sys_ext;
 mod user_namespace;
+mod tmp;
 pub use attach::{attach, AttachOptions};
 pub use exec::{exec, SETCAP_EXE};
 pub use sys_ext::pwnam;

src/tmp.rs
@@ -1,0 +1,44 @@
+use nix::errno::Errno;
+use nix::{Error, Result};
+use std::env;
+use std::ffi::OsString;
+use std::fs;
+use std::os::unix::ffi::OsStringExt;
+use std::path::{Path, PathBuf};
+
+pub struct TempDir {
+    name: Option<PathBuf>,
+}
+impl TempDir {
+    pub fn path(&self) -> &Path {
+        self.name.as_ref().unwrap()
+    }
+    pub fn into_path(mut self) -> PathBuf {
+        self.name.take().unwrap()
+    }
+}
+
+pub fn tempdir() -> Result<TempDir> {
+    let mut template = env::temp_dir();
+    template.push("cntr.XXXXXX");
+    let mut bytes = template.into_os_string().into_vec();
+    // null byte
+    bytes.push(0);
+    let res = unsafe { libc::mkdtemp(bytes.as_mut_ptr().cast()) };
+    if res.is_null() {
+        Err(Error::Sys(Errno::last()))
+    } else {
+        // remove null byte
+        bytes.pop();
+        let name = PathBuf::from(OsString::from_vec(bytes));
+        Ok(TempDir { name: Some(name) })
+    }
+}
+
+impl Drop for TempDir {
+    fn drop(&mut self) {
+        if let Some(ref p) = self.name {
+            let _ = fs::remove_dir_all(p);
+        }
+    }
+}

commit 3b05a730b696b777c84ed635613a05a0ac6a116c

fix permissions of temporary directories
Having them non-readable by non-root users broke for example
breakpointHook in nixpkgs.
This bug was introduced in 4aa6c94b9678fc45f6ee4606dea4365dd77c92da

src/dotcntr.rs
@@ -5,6 +5,10 @@ use nix::unistd::Pid;
 use std::fs::{self, File};
 use std::io::prelude::*;
 use std::os::unix::prelude::*;
+use std::{
+    fs::{set_permissions, Permissions},
+    os::unix::fs::PermissionsExt,
+};
 
 use crate::capabilities;
 use crate::procfs::ProcStatus;
@@ -50,6 +54,12 @@ impl DotcntrDir {
 
 pub fn create(process_status: &ProcStatus) -> Result<DotcntrDir> {
     let dotcntr_dir = tryfmt!(tmp::tempdir(), "failed to create temporary directory");
+    let permissions = Permissions::from_mode(0o755);
+    tryfmt!(
+        set_permissions(dotcntr_dir.path(), permissions),
+        "cannot change permissions of '{}'",
+        dotcntr_dir.path().display()
+    );
     let dotcntr_fd = tryfmt!(
         fcntl::open(
             dotcntr_dir.path(),

src/mountns.rs
@@ -2,11 +2,14 @@ use log::warn;
 use nix::mount::MsFlags;
 use nix::sched::CloneFlags;
 use nix::{cmsg_space, mount, sched, unistd};
-use std::ffi::OsStr;
 use std::fs::{create_dir_all, metadata, remove_dir};
 use std::io;
 use std::os::unix::prelude::*;
 use std::path::{Path, PathBuf};
+use std::{
+    ffi::OsStr,
+    fs::{set_permissions, Permissions},
+};
 
 use crate::fs::CntrFs;
 use crate::ipc;
@@ -41,8 +44,18 @@ impl MountNamespace {
         tryfmt!(mkdir_p(&path), "failed to create /tmp");
 
         let mountpoint = tryfmt!(tmp::tempdir(), "failed to create temporary mountpoint");
+        tryfmt!(
+            set_permissions(mountpoint.path(), Permissions::from_mode(0o755)),
+            "cannot change permissions of '{}'",
+            mountpoint.path().display()
+        );
 
         let temp_mountpoint = tryfmt!(tmp::tempdir(), "failed to create temporary mountpoint");
+        tryfmt!(
+            set_permissions(temp_mountpoint.path(), Permissions::from_mode(0o755)),
+            "cannot change permissions of '{}'",
+            temp_mountpoint.path().display()
+        );
 
         tryfmt!(
             sched::unshare(CloneFlags::CLONE_NEWNS),