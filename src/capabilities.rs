f9acbb03f535796e58917b4087bceda5ea511b6a
Jörg Thalheim <joerg@thalheim.io>
2025-11-08 23:10:01 +0100

    Fix cap drop off-by-one

--- src/capabilities.rs
+++ src/capabilities.rs
@@ -10,7 +10,7 @@ pub(crate) fn drop(inheritable_capabilities: c_ulong, last_cap: c_ulong) -> Resu
     // we need chroot at the moment for `exec` command
     let inheritable = inheritable_capabilities | (1 << CAP_SYS_CHROOT) | (1 << CAP_SYS_PTRACE);
 
-    for cap in 0..last_cap {
+    for cap in 0..=last_cap {
         if (inheritable & (1 << cap)) == 0 {
             // TODO: do not ignore result
             let _ = prctl(libc::PR_CAPBSET_DROP, cap, 0, 0, 0);
