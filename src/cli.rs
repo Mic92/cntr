a06c585aeee973647789110327a848f6c48b5fc5
Jörg Thalheim <joerg@thalheim.io>
2026-08-02 07:58:00 +0200

    Look up users with getpwnam_r again
    Parsing /etc/passwd and shelling out to getent only existed to avoid
    linking libc/NSS. With libc back as a dependency, use the standard
    NSS-aware lookup.

--- src/cli.rs
+++ src/cli.rs
@@ -156,12 +156,7 @@ where
                 match passwd::lookup(&username) {
                     Ok(Some(user)) => effective_user = Some(user),
                     Ok(None) => {
-                        return Err(format!(
-                            "user '{}' not found in /etc/passwd or via getent; \
-                             a numeric uid[:gid] can be used instead",
-                            username
-                        )
-                        .into());
+                        return Err(format!("user '{}' not found", username).into());
                     }
                     Err(e) => {
                         return Err(format!(
